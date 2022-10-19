8d55cad97621f3088de0468f60bd8c72dfd5d870
samuelwigren <samuel.wigren@gsfacket.se>
2022-10-19 21:44:38 +0000

    Add function for o.png and check of clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,49 @@
- /* window.onload = function() {
-    let x = document.createElement("img");
-    x.setAttribute("src", "assets/images/x.png");
-    x.setAttribute("height", "150");
-    x.setAttribute("width", "150");
-    x.setAttribute("alt", "Flower");
-    document.getElementById("boxA").appendChild(x);
-    
-    let o = document.createElement("img");
-    o.setAttribute("src", "assets/images/o.png");
-    o.setAttribute("height", "140");
-    o.setAttribute("width", "140");
-    o.setAttribute("alt", "Flower");
-    document.getElementById("boxC").appendChild(o);
-    }
-*/
+ let playerX = ("x");
+ let playerO = ("o");
+ let boxes = document.getElementsByClassName("grid-item");
+ let clickCounter = 0;
+ console.log(boxes);
 
-   let boxes = document.getElementsByClassName("grid-item");
-    console.log(boxes);
+ function clicked(e) {
+     if (e.currentTarget.hasChildNodes()) {
+         e.currentTarget.style.backgroundColor = 'pink';
+         return;
+     }
+     if (clickCounter % 2 == 0) xClicked(e);
+     else oClicked(e);
+     clickCounter += 1;
+     console.log(clickCounter);
 
- function boxClicked(e) {
-    
-    let box = e.target;
-    let x = document.createElement("img");
-    x.setAttribute("src", "assets/images/x.png");
-    x.setAttribute("height", "150");
-    x.setAttribute("width", "150");
-    x.setAttribute("alt", "X");
-    console.log(box);
-    if (box.innerHTML === "") {
-        box.appendChild(x);
-    }
-    else {
-    box.style.backgroundColor = 'pink';
-    }
-}
+ }
 
-for (let i = 0; i < boxes.length;i++) {
-    let box = boxes[i];
-    console.log(box);
-    
-    box.addEventListener('click', boxClicked);
-}
+ function xClicked(e) {
 
-/* 
-let square = document.getElementById("boxA");
+     let box = e.target;
+     let x = document.createElement("img");
+     x.setAttribute("src", "assets/images/x.png");
+     x.setAttribute("height", "150");
+     x.setAttribute("width", "150");
+     x.setAttribute("alt", "X");
+     console.log(box);
 
-function changeBorder() {
-    square.style.borderColor = "grey";
-}
+     box.appendChild(x);
+ }
 
-function changeBackground() {
-    square.style.backgroundColor = "pink";
-}
+ function oClicked(e) {
 
-function revertBack() {
-    square.style.borderColor = "white";
-    square.style.backgroundColor = "lightblue";
-}
+     let box = e.target;
+     let o = document.createElement("img");
+     o.setAttribute("src", "assets/images/o.png");
+     o.setAttribute("height", "140");
+     o.setAttribute("width", "140");
+     o.setAttribute("alt", "O");
+     console.log(box);
 
-square.addEventListener('click', changeBorder);
-square.addEventListener('mouseover', changeBackground);
-square.addEventListener('mouseleave', revertBack);
-*/
+     box.appendChild(o);
+ }
+
+ for (let i = 0; i < boxes.length; i++) {
+     let box = boxes[i];
+     console.log(box);
+
+     box.addEventListener('click', clicked);
