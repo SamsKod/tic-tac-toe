4f9b7e56b4e009bfaacc474a6b538f804ab220d8
samuelwigren <samuel.wigren@gsfacket.se>
2022-10-24 21:28:11 +0000

    Add js-code checking combinations and html, css and js for a modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,48 +2,133 @@
  let playerO = ("o");
  let boxes = document.getElementsByClassName("grid-item");
  let clickCounter = 0;
- console.log(boxes);
+ let restart = document.getElementById("restart");
+
+ // Listen on restart-button
+ 
+ restart.addEventListener('click', newstart);
+
+ // Restarts the game
+
+ function newstart() {
+     let boxes = document.getElementsByClassName("grid-item");
+     var modal = document.getElementById("myModal");
+     for (let i = 0; i < boxes.length; i++) {
+         let box = boxes[i];
+         box.setAttribute("data-box", "");
+         box.innerHTML = '';
+         clickCounter = 0;
+     }
+     modal.style.display = "none";
+    }
+    
+
+ function checkStatus(player) {
+
+     let winningLines = [
+         [0, 1, 2],
+         [3, 4, 5],
+         [6, 7, 8],
+         [0, 3, 6],
+         [1, 4, 7],
+         [2, 5, 8],
+         [0, 4, 8],
+         [2, 4, 6]
+     ];
+
+     let boxArray = [];
+     for (let j = 0; j < boxes.length; j++) {
+         let boxValue = boxes[j].getAttribute("data-box");
+         if (player == boxValue) {
+             boxArray.push(j);
+         }
+     }
+
+     //Check each winning combination against the board.
+     for (let i = 0; i < winningLines.length; i++) {
+         if (isLineWinning(boxArray, winningLines[i]))
+             return true;
+     }
+
+     return false;
+ }
+
+ function isLineWinning(boxArray, winningLine) {
+     for (let j = 0; j < winningLine.length; j++) {
+         if (!boxArray.includes(winningLine[j])) {
+             return false;
+         }
+     }
+
+     return true;
+ }
+
+
+ // Check if a box has an image, count click and switch between PlayerX and PlayerO
 
  function clicked(e) {
      if (e.currentTarget.hasChildNodes()) {
          e.currentTarget.style.backgroundColor = 'pink';
          return;
      }
-     if (clickCounter % 2 == 0) xClicked(e);
-     else oClicked(e);
+
+     let isLastClick = clickCounter >= 8;
+     if (clickCounter % 2 == 0) xClicked(e,isLastClick);
+     else oClicked(e,isLastClick);
      clickCounter += 1;
-     console.log(clickCounter);
+
 
  }
 
- function xClicked(e) {
+ // Set X-image and attribute when a box is clicked
+
+ function xClicked(e,isLastClick) {
 
      let box = e.target;
+     box.setAttribute("data-box", playerX);
      let x = document.createElement("img");
      x.setAttribute("src", "assets/images/x.png");
      x.setAttribute("height", "150");
      x.setAttribute("width", "150");
      x.setAttribute("alt", "X");
-     console.log(box);
 
      box.appendChild(x);
+     let hasWon = checkStatus("x");
+     if (hasWon == true) openModal("X Won"); 
+     else if (isLastClick == true) openModal("Draw");
+
+
  }
 
- function oClicked(e) {
+ // Set O-image and attribute when a box is clicked
+
+ function oClicked(e,isLastClick) {
 
      let box = e.target;
+     box.setAttribute("data-box", playerO);
      let o = document.createElement("img");
      o.setAttribute("src", "assets/images/o.png");
      o.setAttribute("height", "140");
      o.setAttribute("width", "140");
      o.setAttribute("alt", "O");
-     console.log(box);
 
      box.appendChild(o);
+     let hasWon = checkStatus("o");
+     if (hasWon == true) openModal("O Won"); 
+     else if (isLastClick == true) openModal("Draw");
  }
 
  for (let i = 0; i < boxes.length; i++) {
      let box = boxes[i];
-     console.log(box);
-
      box.addEventListener('click', clicked);
+ }
+
+ // The modal code is copied from w3school.com
+
+// Open modal when game is over
+ function openModal(text) {
+    var modal = document.getElementById("myModal");
+    let p = modal.querySelector("p");
+    p.innerText = text;
+    modal.style.display = "block";
+ }
