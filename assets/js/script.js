0b9060613f513f9982ff8fae4cc673930697736d
samuelwigren <samuel.wigren@gsfacket.se>
2022-10-26 12:29:27 +0000

    Add code to handle small screens

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@
  let restart = document.getElementById("restart");
 
  // Listen on restart-button
- 
+
  restartBtn.addEventListener('click', newstart);
 
  // Restarts the game
@@ -17,11 +17,12 @@
          let box = boxes[i];
          box.setAttribute("data-box", "");
          box.innerHTML = '';
+         box.style = '';
          clickCounter = 0;
      }
      modal.style.display = "none";
-    }
-    
+ }
+
 
  function checkStatus(player) {
 
@@ -63,58 +64,79 @@
      return true;
  }
 
-
  // Check if a box has an image, count click and switch between PlayerX and PlayerO
 
  function clicked(e) {
      if (e.currentTarget.hasChildNodes()) {
-         e.currentTarget.style.backgroundColor = 'pink';
+         e.currentTarget.style.backgroundColor = 'pink'; //If you click twice on same box
          return;
      }
 
      let isLastClick = clickCounter >= 8;
-     if (clickCounter % 2 == 0) xClicked(e,isLastClick);
-     else oClicked(e,isLastClick);
+     if (clickCounter % 2 == 0) xClicked(e, isLastClick);
+     else oClicked(e, isLastClick);
      clickCounter += 1;
-
-
  }
 
  // Set X-image and attribute when a box is clicked
 
- function xClicked(e,isLastClick) {
+ function xClicked(e, isLastClick) {
 
      let box = e.target;
      box.setAttribute("data-box", playerX);
      let x = document.createElement("img");
      x.setAttribute("src", "assets/images/x.png");
-     x.setAttribute("height", "150");
-     x.setAttribute("width", "150");
      x.setAttribute("alt", "X");
 
+     function smallScreen(xScreen) {
+        if (xScreen.matches) { // If media query matches
+            x.setAttribute("height", "100");
+            x.setAttribute("width", "100");
+        } else {
+            x.setAttribute("height", "150");
+            x.setAttribute("width", "150");
+        }
+    }
+
+    //Handle small screen
+
+    let xScreen = window.matchMedia("(max-width: 400px)");
+    smallScreen(xScreen) // Call listener function at run time
      box.appendChild(x);
      let hasWon = checkStatus("x");
-     if (hasWon == true) openModal("X Winns!"); 
-     else if (isLastClick == true) openModal("Draw");
-
+     if (hasWon == true) openModal("X Winns!");
+     else if (isLastClick == true) openModal("It's a Draw");
 
  }
 
  // Set O-image and attribute when a box is clicked
 
- function oClicked(e,isLastClick) {
-
+ function oClicked(e, isLastClick) {
      let box = e.target;
      box.setAttribute("data-box", playerO);
      let o = document.createElement("img");
      o.setAttribute("src", "assets/images/o.png");
-     o.setAttribute("height", "140");
-     o.setAttribute("width", "140");
      o.setAttribute("alt", "O");
+
+     function smallScreen(oScreen) {
+         if (oScreen.matches) { // If media query matches
+             o.setAttribute("height", "90");
+             o.setAttribute("width", "90");
+         } else {
+             o.setAttribute("height", "140");
+             o.setAttribute("width", "140");
+         }
+     }
+
+     //Handle small screen
+
+     let oScreen = window.matchMedia("(max-width: 400px)");
+     smallScreen(oScreen) // Call listener function at run time
+     
 
      box.appendChild(o);
      let hasWon = checkStatus("o");
-     if (hasWon == true) openModal("O Winns!!"); 
+     if (hasWon == true) openModal("O Winns!!");
      else if (isLastClick == true) openModal("It's a Draw");
  }
 
@@ -123,12 +145,12 @@
      box.addEventListener('click', clicked);
  }
 
- // The modal code is copied from w3school.com
+ // Some modal code is copied from w3school.com
 
-// Open modal when game is over
+ // Open modal when game is over
  function openModal(text) {
-    var modal = document.getElementById("myModal");
-    let p = modal.querySelector("p");
-    p.innerText = text;
-    modal.style.display = "block";
+     var modal = document.getElementById("myModal");
+     let p = modal.querySelector("p");
+     p.innerText = text;
+     modal.style.display = "block";
  }
